06a159a28a9c435b270676c78994854f250d9f2d
StarFront <jorgeroncancio0610@gmail.com>
2025-04-24 04:02:30 -0500

    fack2

--- app.py
+++ app.py
@@ -125,4 +125,5 @@ def vender_huevos():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render usa esta variable
+    app.run(host='0.0.0.0', port=port)
